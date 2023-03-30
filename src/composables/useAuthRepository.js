ff0383ec0deece20678ea3f24f4b8baef1e89b74
Yohan Prasetyo <yohanprasetyonugrohoputra@gmail.com>
2023-03-30 14:55:08 +0700

    feat : login

---
+++ src/composables/useAuthRepository.js
@@ -0,0 +1,28 @@
+import { useHttp } from "./useHttp";
+
+export const useAuthRepository = () => {
+  const http = useHttp();
+
+  const login = (credentials) => {
+    return http.post("/api/auth/login", credentials);
+  };
+
+  const register = (credentials) => {
+    return http.post("/api/auth/register", credentials);
+  };
+
+  const logout = () => {
+    return http.get("/api/auth/logout");
+  };
+
+  const profile = () => {
+    return http.get("/api/auth/profile");
+  };
+
+  return {
+    login,
+    register,
+    logout,
+    profile,
+  };
+};
---
+++ src/composables/useHttp.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export const useHttp = () => {
+    const client = axios.create({
+        baseURL: import.meta.env.VUE_APP_API_URL || 'http://localhost:5173',
+        headers: {
+            "Content-Type": "application/json",
+            "X-requested-with": "XMLHttpRequest",
+        },
+    });
+
+    const get = (url, params) => client.get(url, {params}); 
+    const post = (url, params, data) => client.post(url, data, {params}); 
+    const put = (url, data, params) => client.put(url, data, {params}); 
+    const patch = (url, data, params) => client.patch(url, data, {params}); 
+    const destroy = (url, params) => client.delete(url, {params}); 
+
+    return {
+        get,
+        post,
+        put,
+        patch,
+        destroy
+    }
+}
