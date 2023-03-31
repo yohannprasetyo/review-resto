d8b99f6cdc50cb5f0c3c40488cf23bbda3d309d4
Yohan Prasetyo <yohanprasetyonugrohoputra@gmail.com>
2023-03-31 10:40:32 +0700

    fix: localhost http to 8000

--- src/composables/useHttp.js
+++ src/composables/useHttp.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const useHttp = () => {
     const client = axios.create({
-        baseURL: import.meta.env.VUE_APP_API_URL || 'http://localhost:5173',
+        baseURL: import.meta.env.VUE_APP_API_URL || 'http://localhost:8000',
         headers: {
             "Content-Type": "application/json",
             "X-requested-with": "XMLHttpRequest",
